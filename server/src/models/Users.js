0cc73cbf07d9d44e8bccf814c6b29f1325308e2a
hossamgasser <hossgasser@gmail.com>
2025-03-04 01:00:46 -0500

    New Payments db schema
    check comments in user schema

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+
+    // maybe users should have name, email, role, and type as required variables and other variables such as organization (For attendees) and rating (For speaker) as not required.
+    //  When it comes to registration, we intorduce that depending on the user type inputted, specific other variables will be required.
+    // So one database table holding all types of users 
 });
 
 export const UserModel = mongoose.model('users', userSchema);
