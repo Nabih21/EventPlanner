9b1961e9e1d54c6ecb7da4db7e120d0055dbb688
Jad-Aramouni <J_aramou@live.concordia.ca>
2025-03-31 21:12:37 -0400

    Added yazdan's venues file

---
+++ client/event-planner/src/pages/Venues.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Venues = () => {
+  const [venues, setVenues] = useState([]);
+
+  useEffect(() => {
+    const fetchVenues = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/venues");
+        setVenues(response.data);
+      } catch (err) {
+        console.error("Failed to fetch venues:", err);
+      }
+    };
+    fetchVenues();
+  }, []);
+
+  return (
+    <div className="venuesPage">
+      <h1> Venues List</h1>
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Location</th>
+            <th>Capacity</th>
+            <th>Type</th>
+            <th>Available At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {venues.map((venue) => (
+            <tr key={venue._id}>
+              <td>{venue.name}</td>
+              <td>{venue.location}</td>
+              <td>{venue.capacity}</td>
+              <td>{venue.type}</td>
+              <td>{new Date(venue.availability).toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Venues;
