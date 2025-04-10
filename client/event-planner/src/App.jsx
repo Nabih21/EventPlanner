b288dd9689e92d9731f00d7406a322b0ed66489e
Jad-Aramouni <J_aramou@live.concordia.ca>
2025-04-10 15:54:04 -0400

    Revert "Update App.jsx"
    This reverts commit ff61b0ec3266bd1aece794161eb39b58d6286b7c.

--- client/event-planner/src/App.jsx
+++ client/event-planner/src/App.jsx
@@ -1,42 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import React, {useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
 import LandingPage from './pages/LandingPage';
 import Auth from './pages/Auth';
+import Users from './pages/Users';
+import UserDetails from './pages/UserDetails';
 import Events from './pages/Events';
 import Venues from './pages/Venues';
 import EventDetails from './pages/EventDetails';
+import Dashboard from './pages/dashboard';
+import CreateEvent from './pages/CreateEvent';
+
 import './App.css';
-import LiveChat from './components/LiveChat';
 
 function App() {
+const [user, setUser] = useState(null);
 
-  const [isLogin,setLogin] = useState(false);
-
-  useEffect(() => {
-    if(localStorage.getItem("token") != null){
-      setLogin(true);
-    }
-  },[])
   return (
-
-    <>
-    <Router>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/venues" element={<Venues />} />
-          <Route path="/event/:id" element={<EventDetails />} />
-        </Routes>
-      </div>
-    </Router>
-    {isLogin && <LiveChat/>}
-    </>
-
-    
+    <div className="app">
+      <Navbar setUser={setUser}  user={user} />
+      <Routes>
+        <Route path="/" element={<LandingPage user={user} />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/venues" element={<Venues />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/event/:id" element={<EventDetails />} />
+        <Route path="/user/:id" element={<UserDetails />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+      </Routes>
+    </div>
   );
 }
 
